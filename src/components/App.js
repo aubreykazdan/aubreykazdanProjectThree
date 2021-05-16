2bb3268c341c80fefdb42c704c7b8cdfd22af5ef
Aubrey Kazdan <aubrey.j.kazdan@gmail.com>
2021-05-15 21:17:43 -0400

    setting up base functionality + stylings

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import '../styles/App.css';
 import firebase from '../config/firebase.js';
+import header from '../components/header.js';
 import { useEffect, useState } from 'react';
 
 // const apiKey = '';
@@ -7,8 +8,9 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [ goals, setGoals ] = useState([]);
-  // const [ userInput, setUserInput ] = useState('');
+  const [ userInput, setUserInput ] = useState('');
   const dbRef = firebase.database().ref();
+  
   useEffect(() => {
     console.log('This is from useEffect hook');
 
@@ -29,11 +31,35 @@ function App() {
     })
   }, []);
 
+  const handleUserInput = (event) => {
+    let userInput = event.target.value;
+    setUserInput(userInput);
+    console.log(userInput);
+  }
+
+  // submitting the data to Firebase
+  const handleSubmitClick = (event) => {
+    event.preventDefault();
+    // this is submitting the goal to be appended to the component
+    dbRef.push(userInput);
+    // Resetting the input value
+    setUserInput('');
+  }
+
+  const handleRemoveGoal = (goal) => {
+    dbRef.child(goal).remove();
+  }
 
   return (
     <>
       <div className='header'>
         <h1>Mental Health Goals</h1>
+
+        <form action="submit">
+          <label htmlFor="">Your Goals Go Here</label>
+          <input value={userInput} type="text" onChange={handleUserInput} />
+          <button onClick={handleSubmitClick}>Add Goal</button>
+        </form>
 
 
         <ul className="healthChecklist">
@@ -43,12 +69,15 @@ function App() {
               return(
                 <li key={goal.key}>
                   <p>Goal {index + 1}: {goal.objective}</p>
-                  <button>Complete Goal</button>
+                  <button onClick={() => {handleRemoveGoal(goal.key)}}>Remove Goal</button>
                 </li>
               )
             })
           }
         </ul>
+
+        
+        
       </div>
 
     </>
---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+function header(props) {
+    return (
+        <header>
+            <h1>How to Bootcamp: A Mental-Health Survival Checklist</h1>
+        </header>
+    )
+}
+
+
+
+
+export default header;
