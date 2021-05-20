e21e612309b4d78aa02de90550d02e0c00cf03da
Aubrey Kazdan <aubrey.j.kazdan@gmail.com>
2021-05-20 02:50:17 -0400

    project hand in

--- src/components/MainForm.js
+++ src/components/MainForm.js
@@ -1,17 +1,19 @@
 
-function MainForm({userInput, dateInput, inputResponse, dateResponse, handleSubmitClick}) {
+function MainForm({userInput, dateInput, handleGoalInput, handleDateInput, handleSubmitClick}) {
 
 
     return (
         <div className="mainFormContainer">
             <p className="instructions">Input your goals/projects below to see them appear on your Bootcamp to-do list. If you're feeling dangerous, you can also add a due date. Use these tools to help keep track of your progress through Juno's Web Development Bootcamp. </p>
 
+            {/* this form handles everything that is appended to the goal component */}
+
             <form className="mainForm" action="submit" onSubmit={handleSubmitClick}>
                 <label className="enterGoalLabel">Enter Goal Here:</label>
-                <input placeholder="Type here please" value={userInput} type="text" onChange={inputResponse} required/>
+                <input placeholder="Type here please" value={userInput} type="text" onChange={handleGoalInput} required/>
 
                 <label className="dueDateLabel">OPTIONAL: Enter Due Date Here (YYYY-MM-DD):</label>
-                <input placeholder="yyyy-mm-dd" value={dateInput} type="text" name="date" id="date" onChange={dateResponse} />
+                <input placeholder="yyyy-mm-dd" value={dateInput} type="text" name="date" id="date" onChange={handleDateInput} />
 
                 <button className="addGoalButton">Add Goal</button>
             </form>
