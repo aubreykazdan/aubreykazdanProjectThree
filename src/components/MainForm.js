30ed60171ba160b66d3b76b70b8a2f54cbcceb41
Aubrey Kazdan <aubrey.j.kazdan@gmail.com>
2021-05-19 23:21:00 -0400

    responsiveness almost done

--- src/components/MainForm.js
+++ src/components/MainForm.js
@@ -4,13 +4,13 @@ function MainForm({userInput, dateInput, inputResponse, dateResponse, handleSubm
 
     return (
         <div className="mainFormContainer">
-            <h2 className="instructions">Input your goals below to see them appear on your Bootcamp to-do list. If you're feeling dangerous, you can also add a due date. The current date will be at the top of this page to remind you that you're doing okay. </h2>
+            <p className="instructions">Input your goals below to see them appear on your Bootcamp to-do list. If you're feeling dangerous, you can also add a due date. The current date will be at the top of this page to remind you that you're doing okay. </p>
 
             <form className="mainForm" action="submit" onSubmit={handleSubmitClick}>
-                <label>Enter Goal Here:</label>
+                <label className="enterGoalLabel">Enter Goal Here:</label>
                 <input placeholder="Type here please" value={userInput} type="text" onChange={inputResponse} required/>
 
-                <label className="dueDateText">OPTIONAL: Enter Due Date Here (YYYY-MM-DD):</label>
+                <label className="dueDateLabel">OPTIONAL: Enter Due Date Here (YYYY-MM-DD):</label>
                 <input placeholder="yyyy-mm-dd" value={dateInput} type="text" name="date" id="date" onChange={dateResponse} />
 
                 <button className="addGoalButton">Add Goal</button>
